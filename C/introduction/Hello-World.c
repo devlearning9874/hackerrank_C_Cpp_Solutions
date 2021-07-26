85d5a3e74791897109a47acdc36d7c1db3118c1f
devlearning9874 <devlearning9874@gmail.com>
2021-07-26 22:53:20 +0530

    Solved Hello World

---
+++ C/introduction/Hello-World.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+   
+    printf("Hello, World! \n%s", s);   
+    return 0;
+}
+
+// Sample Input 0
+
+// Welcome to C programming.
+// Sample Output 0
+
+// Hello, World!
+// Welcome to C programming.
